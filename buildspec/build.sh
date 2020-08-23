f73609af106185db0bd282e48a192152eb2b914a
John Graham <johngraham660@gmail.com>
2020-08-23 14:57:30 +0100

    Disables awscli template verify check

--- buildspec/build.sh
+++ buildspec/build.sh
@@ -4,5 +4,6 @@ set -e
 
 for filename in cloudformation/*.*; do
     cfn-lint -t ${filename} -i W1020 W2030;
-    aws cloudformation validate-template --template-body file://${filename};
+    # Disabling this check until I figure out the IAM privs for this
+    # aws cloudformation validate-template --template-body file://${filename};
 done
