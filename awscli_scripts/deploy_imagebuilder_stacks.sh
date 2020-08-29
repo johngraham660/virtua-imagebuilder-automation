ff7f7fb3694ffe546be1811f8c22f059d3b31470
John Graham <johngraham660@gmail.com>
2020-08-29 23:49:27 +0100

    Updates

--- awscli_scripts/deploy_imagebuilder_stacks.sh
+++ awscli_scripts/deploy_imagebuilder_stacks.sh
@@ -6,9 +6,24 @@
 
 STACKFILES="centos7-cloudwatch-buildcomponent-stack \
             centos7-update-buildcomponent-stack \
-            rhel7_update_buildcomponent_stack \
+            rhel7-update-buildcomponent-stack \
             rhel7-cloudwatch-buildcomponent-stack"
 
+
+# Debug
+aws cloudformation list-stacks
+
+# ============================
+# Delete buildcomponent stacks
+# ============================
+for stack in ${STACKFILES}
+do
+  aws cloudformation delete-stack --stack-name ${stack}
+done
+
+# ============================
+# Create buildcomponent stacks
+# ============================
 for stack in ${STACKFILES}
 do
   aws cloudformation create-stack --stack-name ${stack} --template-url https://virtua-cloudformation-templates.s3-eu-west-1.amazonaws.com/${stack}.yaml
