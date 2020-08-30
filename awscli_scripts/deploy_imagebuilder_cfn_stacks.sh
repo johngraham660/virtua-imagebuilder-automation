2f699281844f7294bec750a4bc16397c78d4603b
John Graham <johngraham660@gmail.com>
2020-08-30 17:33:04 +0100

    Adds delete & create funtions to stack deploy script

--- awscli_scripts/deploy_imagebuilder_cfn_stacks.sh
+++ awscli_scripts/deploy_imagebuilder_cfn_stacks.sh
@@ -10,21 +10,30 @@ STACKFILES="centos7-cloudwatch-buildcomponent-stack \
             rhel7-update-buildcomponent-stack \
             rhel7-cloudwatch-buildcomponent-stack"
 
-# ============================
-# Delete buildcomponent stacks
-# ============================
-for STACK in ${STACKFILES}
-do
-  echo "Deleting Stack: ${STACK}"
-  aws cloudformation delete-stack --stack-name ${STACK}
-  sleep 5
-done
+function deleteStack() {
+  # ============================
+  # Delete buildcomponent stacks
+  # ============================
+  for STACK in ${STACKFILES}
+  do
+    echo "Deleting Stack: ${STACK}"
+    aws cloudformation delete-stack --stack-name ${STACK}
+    sleep 5
+  done
 
-# ============================
-# Create buildcomponent stacks
-# ============================
-for STACK in ${STACKFILES}
-do
-  echo "Creating Stack: ${STACK}"
-  aws cloudformation create-stack --stack-name ${STACK} --template-url https://${BUCKET}.s3-${REGION}.amazonaws.com/${STACK}.yaml
-done
+}
+
+function createStack() {
+  # ============================
+  # Create buildcomponent stacks
+  # ============================
+  for STACK in ${STACKFILES}
+  do
+    echo "Creating Stack: ${STACK}"
+    aws cloudformation create-stack --stack-name ${STACK} --template-url https://${BUCKET}.s3-${REGION}.amazonaws.com/${STACK}.yaml
+  done
+
+}
+
+deleteStack
+#createStack
