6ac992b169a6dfd043d0bad50dd2a33986a14df9
John Graham <johngraham660@gmail.com>
2020-08-31 19:56:34 +0100

    Updates deploy_imagebuilder_cfn_templates script with new usage function

--- awscli_scripts/deploy_imagebuilder_cfn_stacks.sh
+++ awscli_scripts/deploy_imagebuilder_cfn_stacks.sh
@@ -3,12 +3,7 @@
 # =================================================
 # Deploy the EC2ImageBuilder Cloudformationn Stacks
 # =================================================
-REGION="eu-west-1"
-BUCKET="virtua-cloudformation-templates"
-STACKFILES="centos7-cloudwatch-buildcomponent-stack \
-            centos7-update-buildcomponent-stack \
-            rhel7-update-buildcomponent-stack \
-            rhel7-cloudwatch-buildcomponent-stack"
+
 
 function deleteStack() {
   # ============================
@@ -35,5 +30,76 @@ function createStack() {
 
 }
 
-deleteStack
-#createStack
+function usage() {
+  echo "Usage:"
+  echo "  --create, -c : create stack"
+  echo "  --delete, -d : delete stack"
+  echo "  --help,   -h : Show this usage message"
+}
+
+# ====
+# Main
+# ====
+
+# =======================================
+# Check the user supplied a vailid input.
+# =======================================
+main() {
+
+  REGION="eu-west-1"
+  BUCKET="virtua-cloudformation-templates"
+
+  if [[ -z ~/.aws/credentials ]]
+  then
+    echo "Cannot find credentials file to connect to AWS"
+    exit 1
+  else
+    # We found credentials but no idea if they work
+    STACKFILES=$(aws s3 ls s3://${BUCKET} | grep buildcomponent-stack.yaml | awk '{print $NF}')
+    echo "DEBUG: ${STACKFILES}"
+    echo "DEBUG: ${BUCKET}"
+
+    if [ -z ${STACKFILES} ]
+    then
+      # STACK files is empty, either way lets exit as we have no templates
+      echo "Cannot identify the CloudFormation template names"
+      exit 1
+    fi
+  fi
+
+  if [[ $# -eq 0 ]]
+  then
+    usage
+    exit 1
+  fi  
+  while test $# -gt 0
+  do
+    case "$1" in
+      --create)
+        createStack
+        ;;
+      --delete)
+        deleteStack
+        ;;
+      --help)
+        usage
+        ;;
+      -c)
+        createStack
+        ;;
+      -d)
+        deleteStack
+        ;;
+      -h)
+        usage
+        ;;
+      *)
+        echo "Option $1 is not recognised"
+        exit 1
+        ;;
+    esac
+    shift
+  done
+}
+
+main "$@"
