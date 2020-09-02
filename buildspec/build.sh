956694360a97d03890577d14da76dcdda75dd15a
John Graham <johngraham660@gmail.com>
2020-09-02 23:09:57 +0100

    Removes the ignore checks in the validate build steps

--- buildspec/build.sh
+++ buildspec/build.sh
@@ -3,7 +3,7 @@
 set -e
 
 for filename in cloudformation/**/*.yaml; do
-    cfn-lint -t ${filename} -i W1020 W2030;
+    cfn-lint -t ${filename}
     # Disabling this check until I figure out the IAM privs for this
     # aws cloudformation validate-template --template-body file://${filename};
 done
